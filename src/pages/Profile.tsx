b1bed03640713c7e0fd381a44f39f256bb415032
Darllancabral <cabraldarllan@gmail.com>
2026-05-12 15:10:12 -0300

    profile1.2

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -5,50 +5,52 @@ import { Heart, MapPin, Settings, Star } from 'lucide-react';
 export default function Profile() {
   return (
     <>
-      <div className='bg-slate-50'  >
-        <div className='flex justify-between'>
-         <h1>Lucas Andrae</h1>
-          <Settings />
-          <button>Editar parfil</button>
-        </div>
+      <div className='m-6'>
 
-        <div>
-          <div>
+        <div className='flex items-center justify-between w-full'>
+
+          <h1 className='text-xl'>Lucas Andrae</h1>
+
+          <div className='flex border rounded-2xl items-center'>
+
+            <Settings />
+            <button className='px-4 py-1 cursor-pointer'>Editar perfil</button>
+          </div>
+
+        </div>
+        <div className='flex gap-4'>
+          <div className='flex  w-40 h-30  flex-col items-center gap-3 border p-4 rounded-xl'>
             <MapPin />
             <h2>23</h2>
-            <h2>Locais visitddddddsaddasdsadados</h2>
+            <p>Locais visitados</p>
           </div>
 
-          <div>
+          <div className='flex w-40 h-30 flex-col items-center gap-3 border p-4 rounded-xl'>
             <Heart />
             <h2>4</h2>
             <h2>Favoritos</h2>
           </div>
 
-          <div>
+          <div className='flex w-40 h-30 flex-col items-center gap-3 border p-4 rounded-xl'>
             <Star />
             <h2>8</h2>
             <h2>Avaliações</h2>
           </div>
 
         </div>
-        <div>
-          <button>
+
+        <div className='flex p-4 w-full h-10 mt-8 gap-4 shadow bg-gray-200 rounded-xl items-center'>
+          <div className='flex items-center shadow bg-white w-40  h-8 rounded-xl cursor-pointer'>
             <Heart />
-            <h2>4</h2>
             <h2>Favoritos</h2>
-          </button>
+          </div>
 
-          <button>
+          <div className='flex items-center shadow bg-white w-40  h-8 rounded-xl cursor-pointer'>
             <Star />
-            <h2>8</h2>
             <h2>Avaliações</h2>
-          </button>
+          </div>
+
         </div>
-
-
-
-
 
       </div>
 
