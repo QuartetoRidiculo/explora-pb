932d894e4517213b17e227132c55124915227b25
Darllancabral <cabraldarllan@gmail.com>
2026-05-12 17:17:41 -0300

    profile/cards

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -1,5 +1,5 @@
 import { Heart, MapPin, Settings, Star } from 'lucide-react';
-
+import Card from "../components/Card";
 
 
 
@@ -10,16 +10,16 @@ export default function Profile() {
 
         <div className='flex items-center justify-between w-full'>
 
-          <h1 className='text-xl'>Lucas Andrae</h1>
+          <h1 className='text-xl font-bold '>Lucas Andrae</h1>
 
           <div className='flex border rounded-2xl items-center'>
 
             <Settings />
-            <button className='px-4 py-1 cursor-pointer'>Editar perfil</button>
+            <button className='px-4 py-1 cursor-pointerfont-bold'>Editar perfil</button>
           </div>
 
         </div>
-        <div className='flex gap-4'>
+        <div className='flex gap-4 pt-8'>
           <div className='flex  w-40 h-30  flex-col items-center gap-3 border p-4 rounded-xl'>
             <MapPin />
             <h2>23</h2>
@@ -52,6 +52,45 @@ export default function Profile() {
           </div>
 
         </div>
+        
+     <div className='flex gap-4 pt-8'>
+
+        <Card
+          props={{
+            href: "/local/1",
+            rating: 4.8,
+            title: "Praia de Tambaba",
+            location: "Conde - PB",
+            imgUrl: "https://cdn.jornaldaparaiba.com.br/wp-content/uploads/2023/01/BAIA-DA-TRAICAO.png",
+            qtdReviews: 120,
+          }}
+        />
+
+        <Card
+          props={{
+            href: "/local/2",
+            rating: 4.5,
+            title: "Lajedo de Pai Mateus",
+            location: "Cabaceiras - PB",
+            imgUrl: "https://files.clickpb.com.br/wp-content/uploads/2023/12/31134503/lajedo-de-pai-mateus.jpg",
+            qtdReviews: 87,
+          }}
+        />
+
+        <Card
+          props={{
+            href: "/local/3",
+            rating: 4.3,
+            title: "Parque do Povo",
+            location: " Campina Grande - PB",
+            imgUrl:"https://www.segueviagem.com.br/wp-content/uploads/2022/03/Parque-do-Povo-Campina-Grande-Paraiba-Credito-editorial-Cacio-Murilo-shutterstock_1954331074.jpg",
+            qtdReviews: 120,
+          }}
+          />
+
+      </div>
+
+
 
       </div>
 
